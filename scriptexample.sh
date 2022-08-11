38630514526fb0b04dbd5e46ffc674c859d20a9a
raznerGittyUp <davidrazner@gmail.com>
2022-08-11 14:15:55 -0600

    add weather report

--- scriptexample.sh
+++ scriptexample.sh
@@ -3,51 +3,71 @@
 type_out () {
   message="$1"
   for i in $(seq 0 $(expr length "${message}")) ; do
-    echo -n "${message:$i:1}"
-    sleep 0.05
+    letter=${message:$i:1}
+    echo -n "$letter"
+    case $letter in
+      [[:space:]])
+        sleep $(( ( RANDOM % 10 ) / 8 ));;
+    esac
+    sleep $(( ( RANDOM % 10 ) / 10 ))
   done
   echo ""
 }
 
+resize -s 90 150
 clear
 
 name=$1
+city=$2
+
 if [ -z "$name" ]
 	then
 		type_out "Who are you? ... "
 		read name
 		clear
 		sleep 1
-    type_out "Hello $name"
+    type_out "Hello $name."
 else
-	type_out "Thanks for providing your name, $name"
+	type_out "Thanks for providing your name, $name."
+fi
+
+if [ -z "$city" ]
+	then
+		city="Denver"
 fi
 
 echo ""
 sleep 1
 day="$(date +%a)"
 day_number="$(date +%d)"
-ending="th."
+ending="th"
 
 case $((day_number)) in
-  1) ending="st.";;
-  2) ending="nd.";;
-  3) ending="rd.";;
+  1) ending="st";;
+  2) ending="nd";;
+  3) ending="rd";;
 esac
 
+type_out "It's $day the $day_number$ending,"
+
 case $day in
-	Sat|Sun) type_out "Happy Weekend";;
-	Mon|Tue) type_out "Lets start this week strong";;
-	Thu) type_out "It's almost the weekend";;
-	Wen) type_out "The week is coming along";;
+	Sat|Sun) type_out "nice, the Weekend!";;
+	Mon|Tue) type_out "lets start this week strong.";;
+	Thu) type_out "it's almost the weekend.";;
+	Wen) type_out "the week is coming along.";;
 	Fri)
 		if (($((day_number)) > 7 && $((day_number)) < 14)); then
-			type_out "Happy Focus Friday, Zendesk!"
+			type_out "Happy Recharge Friday, Zendesk!"
 		else
-			type_out "Happy Friday the $day_number$ending"
+			type_out "Happy Friday!"
 		fi;;
 esac
 
-type_out "On this day: $day the $day_number$ending"
-sleep 2
+echo ""
+sleep 0.5
+type_out "Here is your weather forecast, for $city: "
+echo ""
+sleep 0.5
+
+curl wttr.in/$city?u
 
